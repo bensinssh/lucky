ee33a49c1b72a1a8f992db05007ac4c30c8e9fcc
menuflex <79350810+bensinssh@users.noreply.github.com>
2021-11-11 21:50:49 +0700

    Update and rename app(1).py to app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from linebot import (
 
 app = Flask(__name__)
 
-lineaccesstoken = 'sVi63a9na79V/b+yduVI4yExkvFLcsZGHpCNgiDRnJVNdlWG22i5ICYiEfSmyX3o0ES4ZZ268XYaGETDPlSEu6htUND4nMeICcbHDvUoj3JHlLO0ZQBLh26jgoWOOk6moTB3eRp8U0+wBHbt54SZQwdB04t89/1O/w1cDnyilFU='
+lineaccesstoken = ACCESS_TOKEN2''
 line_bot_api = LineBotApi(lineaccesstoken)
 
 casedata = pd.read_excel('casedata.xlsx')
